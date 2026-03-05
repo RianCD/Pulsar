de82834773f727ea7dda715259fafcd5d8cd2c3d
Rian Cardoso Dourado <rian10cardosi@gmail.com>
2026-03-05 00:03:04 -0300

    feat: bootstrap spring boot project and configure supabase connection

---
+++ pulsar/src/test/java/com/riancd/io/pulsar/PulsarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riancd.io.pulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

45638e93ea479b10075a468eff081246d1bdb893
Rian <rian10cardosi@gmail.com>
2026-07-03 13:36:24 -0300

    fix: downgrade hibernate-vector, remove lombok and update properties

--- pulsar/src/main/java/com/riancd/io/pulsar/model/News.java
+++ pulsar/src/main/java/com/riancd/io/pulsar/model/News.java
@@ -1,24 +1,20 @@
 package com.riancd.io.pulsar.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "tb_news")
-@Data
-@NoArgsConstructor
 public class News {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // gera os ID automaticamente
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
-    @Column(columnDefinition = "TEXT") // O tipo TEXT permite textos longos
+    @Column(columnDefinition = "TEXT")
     private String rawContent;
 
     @Column(columnDefinition = "TEXT")
@@ -30,4 +26,54 @@ public class News {
     @Column(columnDefinition = "vector(384)")
     private float[] embedding;
 
+    public News() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRawContent() {
+        return rawContent;
+    }
+
+    public void setRawContent(String rawContent) {
+        this.rawContent = rawContent;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(String sentiment) {
+        this.sentiment = sentiment;
+    }
+
+    public float[] getEmbedding() {
+        return embedding;
+    }
+
+    public void setEmbedding(float[] embedding) {
+        this.embedding = embedding;
+    }
 }
